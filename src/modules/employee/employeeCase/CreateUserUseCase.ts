21a0002157f038f4e9e1d096ee3d29ae323b1217
unknown <teteulima@outlook.com>
2024-05-13 22:04:37 -0300

    updated name of table in db

---
+++ src/modules/employee/employeeCase/CreateUserUseCase.ts
@@ -0,0 +1,9 @@
+import { prisma } from "../../../prisma/client";
+import { User } from "../types/UserType";
+
+export class EmployeeService {
+    async ShowEmployees () {
+        
+    } 
+}
+    
--- src/modules/services/serviceCases/ServiceCases.ts
+++ src/modules/services/serviceCases/ServiceCases.ts
@@ -3,12 +3,12 @@ import { prisma } from "../../../prisma/client";
 export class ServiceCases {
     async show() {
         // número total de serviços para a logica de pegar 3 serviços aleatoriamente
-        const totalCountService = await prisma.services.count();
+        const totalCountService = await prisma.service.count();
 
         // Sempre subtrai 3 para garantir que em todas as ocasiões vai ter pelo menos 3 serviços
         const randomOffset = Math.floor(Math.random() * (totalCountService - 3  ));
 
-        const services = await prisma.services.findMany({
+        const services = await prisma.service.findMany({
             skip: randomOffset,
             take: 3
         });
@@ -19,7 +19,7 @@ export class ServiceCases {
     async findByName(name:string, page:number, perPage:number) {
         const offset = (page -1) * perPage
 
-        const service = await prisma.services.findMany({
+        const service = await prisma.service.findMany({
             where: {
                 name: {
                     contains: name.toLowerCase()
